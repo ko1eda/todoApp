8b78505bc028a7da872c92ffe520a9f106110dc4
christopher-koleda <koleda.christopher@gmail.com>
2018-01-03 12:32:36 -0500

    added completed task and delete functionality to js file.

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,4 +1,21 @@
+var debug = true;
+/**
+* Adds click listener to each li list element.
+* Toggles .completedTodo class on and off
+*/
+$("li").on("click",function(){
+	$(this).toggleClass("completedTodo");
+});
 
+/**
+* Ads click listner to each span of class delete
+* Stops the bubbling of events which prevents completed class from being added
+* Removes the parent li element from the ul.
+*/
+$(".delete").on("click",function (event) {
+	event.stopPropagation();
+	$(this).parent().remove();
+});
 
 
 
