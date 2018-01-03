928d106de514e221369e54aa80f9a6fc70c29f95
christopher-koleda <koleda.christopher@gmail.com>
2018-01-03 17:24:02 -0500

    added input text functionality, fixed listeners to apply to newly added inputs.

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,20 +1,42 @@
 var debug = true;
+
+
 /**
 * Adds click listener to each li list element.
 * Toggles .completedTodo class on and off
 */
-$("li").on("click",function(){
+$("ul").on("click","li",function(){
 	$(this).toggleClass("completedTodo");
 });
 
 /**
-* Ads click listner to each span of class delete
-* Stops the bubbling of events which prevents completed class from being added
-* Removes the parent li element from the ul.
+* Adds click listner to the UL container, second argument specifies 
+*	that the listner is only added when a .delete class element is clicked.
+* 	When an element of class .delete is pressed, get the parent element (<li>),
+* 	fade it out, then remove the li element in the callback function after fade.
+* .stopPropogation(): Stops the bubbling of events which prevents completed class from being added
 */
-$(".delete").on("click",function (event) {
+$("ul").on("click",".delete",function (event) {
 	event.stopPropagation();
-	$(this).parent().remove();
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove(); //$(this) is referring to the li element, not the span b/c the li is the parent element
+	});
+});
+
+
+/**
+* Selects the text input and adds a keypress listener specifically for enter key.
+*	If Enter: get the text value of input, store it and clear the text value.
+*	Append the newly entered text to the UL list as an li, add in span class tag for delete.
+*/
+$("input[type =\"text\"]").keypress(function(event) {
+		if(event.which === 13){
+			//get value from textarea & clear it
+			var inputText = $(this).val();
+			$(this).val("");
+			//add item to todo list
+			$("ul").append("<li><span class=\"delete\">X </span>" +inputText+ "</li>");
+		}
 });
 
 
